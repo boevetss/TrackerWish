251e90594b56883de05c1fe0a29dd48036ceed41
boevetss <73750523+boevetss@users.noreply.github.com>
2022-04-28 01:08:48 +0300

    добавлена страница каталога, списка желаний и необходимые ресурсы

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route("/SignIN")
 def signin():
     return render_template('signin.html')
-
 
 @app.route("/SignUp")
 def signup():
     return render_template('signup.html')
 
+@app.route("/Catalog")
+def catalog():
+    return render_template('catalog.html')
+
+@app.route("/Wish")
+def wish():
+    return render_template('wish.html')   
 
 if __name__ == "__main__":
     app.run()
