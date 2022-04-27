74007b84082c287d8d6f2502f6cde2e34a9e86bb
Anna Chukhrova <nyasha1999@mail.ru>
2022-04-27 21:46:05 +0300

    + add action - push button;

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def index():
+    return render_template('index.html')
+
+
+@app.route("/SignIN")
+def signin():
     return render_template('signin.html')
+
+
+@app.route("/SignUp")
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == "__main__":
     app.run()
